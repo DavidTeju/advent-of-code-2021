ac051b8bc6d2f8fe76041650a0bc0466ee0eb385
DavidTeju <61572668+DavidTeju@users.noreply.github.com>
2021-12-04 02:48:39 -0600

    added better binary converter

--- src/day3/Day3.java
+++ src/day3/Day3.java
@@ -1,7 +1,6 @@
 package day3;
 
 import java.io.File;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Day3 {
@@ -12,9 +11,9 @@ public class Day3 {
 
     public static int part1() throws Exception {
         final Scanner sc = new Scanner(new File("src\\day3\\input.txt"));
-        String gamma = "";
+        StringBuilder gamma = new StringBuilder();
         int intGamma = 0;
-        String epsilon = "";
+        StringBuilder epsilon = new StringBuilder();
         int intEpsilon = 0;
         String nextValue;
         int[] dominantCounter = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -33,23 +32,45 @@ public class Day3 {
 
         for (int bit : dominantCounter) {
             if (bit > 0){
-                gamma += "1";
-                epsilon += "0";
+                gamma.append("1");
+                epsilon.append("0");
             }else if (bit<0) {
-                gamma += "0";
-                epsilon += "1";
+                gamma.append("0");
+                epsilon.append("1");
             }else
                 throw  new Exception("Challenge doesn't specify what to do if equal values");
         }
 
         /* alternative method to convert String gamma in binary form to int intGamma in Decimal
+        * Much more concise but harder to read and slower at ~721 ms
         for (int i = gamma.length(), j = 0; i>0; i--, j++)
             intGamma += Integer.parseInt(""+gamma.charAt(j)) * (int) Math.pow(2, i-1);
 
         for (int i = epsilon.length(), j = 0; i>0; i--, j++)
-            intEpsilon += Integer.parseInt(""+epsilon.charAt(j)) * (int) Math.pow(2, i-1);
+            intEpsilon += Integer.parseInt(""+epsilon.charAt(j)) * Math.pow(2, i-1);
             */
 
+        long binaryGamma = Long.parseLong(""+gamma);
+        long binaryEpsilon = Long.parseLong(""+epsilon);
+        int n = 0;
+        //This method is faster and saves time with ~404 ms
+        while(binaryGamma > 0)
+        {
+            long temp = binaryGamma%10;
+            intGamma += temp*Math.pow(2, n);
+            binaryGamma = binaryGamma/10;
+            n++;
+        }
+        n = 0;
+
+        while(binaryEpsilon > 0)
+        {
+
+            long temp = binaryEpsilon%10;
+            intEpsilon += temp*Math.pow(2, n);
+            binaryEpsilon = binaryEpsilon/10;
+            n++;
+        }
 
         return intGamma*intEpsilon;
     }
