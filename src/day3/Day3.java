ed6f684b7ca074300616e063a85a01126a1d82fe
DavidTeju <tejuoshodavid@gmail.com>
2021-12-17 06:46:24 -0600

    Completed Day 3

--- src/day3/Day3.java
+++ src/day3/Day3.java
@@ -1,8 +1,8 @@
 package day3;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Day3 {
@@ -10,50 +10,37 @@ public class Day3 {
     public static void main(String[] args) {
         try {
             System.out.println(part1());
+            System.out.println(partTwo());
         } catch (Exception e) {
+            System.out.println("It's a main error");
             e.printStackTrace();
         }
     }
 
-    static int part1() throws Exception {
+    static long part1() throws Exception {
         final Scanner sc = new Scanner(new File("src\\day3\\input.txt"));
-        StringBuilder gamma = new StringBuilder();
-        StringBuilder epsilon = new StringBuilder();
+        String gamma = "";
+        String epsilon = "";
         String nextValue = sc.next();
         int[] dominantCounter = new int[nextValue.length()];
-        boolean toContinue;
 
-        /*
-        The point of this loop is to go through each number in the file, and go through each bit of each number
-        If the value of the first bit is 1, I add to the first int of the dominantCounter array
-        If the value is 0, I subtract 1 from the corresponding int of the dominicanCounter array instead
-        */
-        int x = 0;
-        do {
-            toContinue = sc.hasNext();
-            for (int i = 0; i < nextValue.length(); i++) {
-                if (nextValue.charAt(i) == '0')
-                    dominantCounter[i]--;
-                else if (nextValue.charAt(i) == '1')
-                    dominantCounter[i]++;
-                else
-                    throw new IllegalArgumentException("invalid input detected. All input must be in binary");
-            }
-            if (toContinue)
-                nextValue = sc.next();
-        } while (toContinue);
+        analyseNumber(dominantCounter, nextValue);
+
+        while (sc.hasNext()) {
+            analyseNumber(dominantCounter, nextValue);
+            nextValue = sc.next();
+        }
 
         for (int bit : dominantCounter) {
             if (bit > 0){
-                gamma.append("1");
-                epsilon.append("0");
+                gamma+="1";
+                epsilon+="0";
             }else if (bit<0) {
-                gamma.append("0");
-                epsilon.append("1");
+                gamma+="0";
+                epsilon+="1";
             }else
                 throw  new Exception("Challenge doesn't specify what to do if equal values");
         }
-
         /* alternative method to convert String gamma in binary form to int intGamma in Decimal
         * Much more concise but harder to read and slower
         int intGamma = 0;
@@ -68,8 +55,28 @@ public class Day3 {
         return convertBinary(epsilon)*convertBinary(gamma);
     }
 
-    static int convertBinary(StringBuilder toConvert){
-        long binary = Long.parseLong(""+toConvert);
+    /**
+     Iterates through each bit of nextValue.
+     <p>If the value of the Nth bit is 1, it adds to the Nth int of the dominantCounter array</p>
+     <p>If the value is 0, It subtracts 1 from the corresponding int of the dominicanCounter array instead</p>
+     <p>This allows us to end up with an array where, for each value, a positive number means the bit, 1, appears more times than 1</p>
+     <p>Meanwhile, a negative number means the bit, 0, appears more times than 0.</p>
+     @param dominantCounter Takes the array(mutable) and modifies
+     @param nextValue Is the number to analyse
+     */
+    static void analyseNumber(int[] dominantCounter, String nextValue) {
+        for (int n = 0; n < nextValue.length(); n++) {
+            if (nextValue.charAt(n) == '0')
+                dominantCounter[n]--;
+            else if (nextValue.charAt(n) == '1')
+                dominantCounter[n]++;
+            else
+                throw new IllegalArgumentException("invalid input detected. All input must be in binary");
+        }
+    }
+
+    static long convertBinary(String toConvert){
+        long binary = Long.parseLong(toConvert);
         int n = 0;
         long temp;
         int ternary = 0;
@@ -83,26 +90,51 @@ public class Day3 {
         return ternary;
     }
 
-//    static int partTwo() throws Exception {
-//        final Scanner sc = new Scanner(new File("src\\day3\\input.txt"));
-//        String nextValue;
-//        int dominantCounter = 0;
-//nextValue = sc.next();
-//        while (sc.hasNext()){
-//            nextValue = sc.next();
-//            if (nextValue.charAt(position) == '0')
-//                dominantCounter--;
-//            else if (nextValue.charAt(i) == '1')
-//                dominantCounter++;
-//            else
-//                throw new IllegalArgumentException("invalid input detected. All input must be in binary");
-//
-//        }
-//
-//        return 0;
-//    }
-//
-//    static int scrubberRating (String element, ArrayList<Integer> inConsideration, int position){
-//        return 0;
-//    }
+    static long partTwo() throws Exception {
+        final Scanner sc = new Scanner(new File("src\\day3\\input.txt"));
+        List<String> fullInput = new ArrayList<>();
+        while (sc.hasNext())
+            fullInput.add(sc.next());
+
+        return
+                convertBinary(rating("oxygen", fullInput, 0).replace("\n", "")) *
+                        convertBinary(rating("CO2", fullInput, 0).replace("\n", ""));
+    }
+
+    static String rating(String element, List<String> toAnalyze, int position){
+        if (toAnalyze.size()==1)//If there is only one number left, it returns that number
+        {
+            System.out.println(toAnalyze.get(0));
+            return toAnalyze.get(0);
+        }
+
+        List<List<String>> lists = new ArrayList<>();
+        lists.add(new ArrayList<>());
+        lists.add(new ArrayList<>());
+        List<String> toPass;
+
+        for (String nextValue: toAnalyze){
+            if (nextValue.charAt(position) == '0') {
+                lists.get(0).add(nextValue);
+            }else if (nextValue.charAt(position) == '1') {
+                lists.get(1).add(nextValue);
+            }else {
+                throw new IllegalArgumentException("invalid input detected. All input must be in binary");
+            }
+        }
+
+
+        int dominant;
+        if (lists.get(0).size()>lists.get(1).size())
+            dominant = 0;
+        else
+            dominant = 1;
+
+        if (element.equals("oxygen"))
+            toPass = lists.get(dominant);
+        else
+            toPass = lists.get((dominant+1)%2);
+
+        return rating(element, toPass, ++position);
+    }
 }
